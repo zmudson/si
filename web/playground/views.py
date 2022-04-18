5b4957e9cdc8d53febb2251ec2fadcbeb7c6d32d
Dominik Zmuda-Trzebiatowski <dominik.zmuda_trzebiatowski@wp.pl>
2022-04-19 01:07:00 +0200

    handle form

--- web/playground/views.py
+++ web/playground/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import jsonpickle
 import pandas as pd
-
 from playground.models import Season, Team, Match
+
+jsonpickle.set_encoder_options('simplejson',
+                               use_decimal=True, sort_keys=True)
 
 seasons = list()
 
@@ -12,9 +14,9 @@ PATH = "data\\preseason-odds\\"
 
 for i in range(8, 21):
     if i == 8:
-        READPATH = PATH + str(2000+i) + '-0' + str(i+1) + '-odds.csv'
+        READPATH = PATH + str(2000 + i) + '-0' + str(i + 1) + '-odds.csv'
     else:
-        READPATH = PATH + str(2000+i) + '-' + str(i+1) + '-odds.csv'
+        READPATH = PATH + str(2000 + i) + '-' + str(i + 1) + '-odds.csv'
     odds = pd.read_csv(READPATH)
     teams = odds.Team
     values = odds.Odds
@@ -29,12 +31,12 @@ for i in range(8, 21):
         season_teams.append(Team(teams[j], values[j]))
     season_teams.sort(key=lambda x: x.name)
 
-    seasons.append(Season(str(2000+i) + '/' + str(2000 + i + 1), season_teams))
+    seasons.append(Season(str(2000 + i) + '/' + str(2000 + i + 1), season_teams))
 
 PATH = "data\\games\\"
 
 for i in range(8, 21):
-    READPATH = PATH + str(2000+i) + '-' + str(2000+i+1) + '.csv'
+    READPATH = PATH + str(2000 + i) + '-' + str(2000 + i + 1) + '.csv'
     games = pd.read_csv(READPATH)
 
     dates = games.Date
@@ -52,19 +54,17 @@ for i in range(8, 21):
     for j in range(len(dates)):
         home = None
         away = None
-        for team in seasons[i-8].teams:
+        for team in seasons[i - 8].teams:
             if team.name == home_teams[j]:
                 home = team
             if team.name == away_teams[j]:
                 away = team
         if not home or not away:
             continue
-        season_games.append(Match(dates[j], home, away, home_scores[j], away_scores[j], home_odds[j],
-                                  away_odds[j], winners[j], playoffs[j], preseasons[j]))
+        season_games.append(Match(dates[j], home, away, str(home_scores[j]), str(away_scores[j]), home_odds[j],
+                                  away_odds[j], str(winners[j]), str(playoffs[j]), str(preseasons[j])))
 
-
-    seasons[i-8].matches = season_games
-
+    seasons[i - 8].matches = season_games
 
 
 def hello(request):
@@ -80,3 +80,10 @@ class SeasonView(APIView):
         period = request.GET.get('period', '')
         season = next(season for season in seasons if season.period == period)
         return Response(jsonpickle.encode(season, unpicklable=False))
+
+
+class ResultView(APIView):
+    def post(self, request):
+        match = jsonpickle.decode(request.body.decode('utf-8'))
+        winner = match['home_team'] if match['winner'] == '0' else match['away_team']
+        return Response(jsonpickle.encode(winner, unpicklable=False))  # return winner
